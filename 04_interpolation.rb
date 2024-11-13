bf44b60aff28ede2b8c511384e40b54890d4224c
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-11-13 12:02:43 +0530

    Interpolation

---
+++ 04_interpolation.rb
@@ -0,0 +1,11 @@
+# Math: String Interpolation
+
+=begin
+  Adding variables to a string
+=end
+
+name = "Rahul"
+
+puts "Hello, #{name}!"
+puts 'Hello, #{name}!'  # Does not work in single quotes
+puts "Hello, " + name + "!"
