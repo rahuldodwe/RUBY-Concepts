bbb2dd2721d70fdababc99f97a81ef8efcb7793e
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-11-13 11:56:22 +0530

    Arithmetic Operators

---
+++ 03_arithmetic_operators.rb
@@ -0,0 +1,38 @@
+# Math: Arithmetic Operators
+
+=begin
+  + addition
+  - substraction
+  * multiplication
+  / division
+  % modulo (remainder)
+ ** exponential (power)
+=end
+
+a = 7
+b = 2
+
+puts a + b 
+#o/p: 9
+
+puts a - b 
+#o/p: 5
+
+puts a * b 
+#o/p: 14
+
+puts a % b 
+#o/p: 1
+
+puts a ** b 
+#o/p: 49
+
+puts a / b 
+#o/p: 3
+
+
+c = 9.0
+d = 4
+
+puts c/d 
+# Output: 2.25
