c67acb1e696ab2705ec3be00458807e90f8e223e
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-11-13 12:53:00 +0530

    Assignment Operator

---
+++ 09_assignment_operatos.rb
@@ -0,0 +1,22 @@
+# Assignment Operatos
+
+=begin
+  Assignment Operators:
+  += , -= , *= , /=
+=end
+
+a = 100
+b = 10
+puts a += b  # a = a + b
+
+a = 100
+b = 10
+puts a -= b  # a = a - b
+
+a = 100
+b = 10
+puts a *= b  # a = a * b
+
+a = 100
+b = 10
+puts (a /= b)  # a = a / b
