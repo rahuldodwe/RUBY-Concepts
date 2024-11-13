4f6bbd66ea7d1488514952a9515a91556f1b78d4
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-11-13 11:42:37 +0530

    print vs puts

---
+++ 01_print_vs_puts.rb
@@ -0,0 +1,25 @@
+#print vs puts
+
+# print "Hello World"
+# puts "How are you"
+# print "Bye World"
+# puts "End"
+
+
+# Predict output of these examples
+#  Example 1
+puts "Hello"
+print "world"
+
+
+# # Example 2
+# print "Hello"
+# puts "World"
+
+# # Example 3
+# print "Hello"
+# print "World"
+
+# # Example 4
+# puts "Hello"
+# puts "World"
