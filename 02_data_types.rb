d2196fab83f64fa9f06f0fd0042e80f4620554e3
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-11-13 11:48:44 +0530

    data types

---
+++ 02_data_types.rb
@@ -0,0 +1,18 @@
+# Data Types
+
+=begin
+ Data types in ruby are: String, Number, Boolean,
+  Nil, Arrays, Hashes, Symbols 
+=end
+
+puts "\n"
+
+my_num = 25             # Integer
+my_boolean = true       # Boolean
+my_string = "Ruby"      # String
+my_float = 3.14         # Float/Decimal
+
+puts my_num
+puts my_boolean
+puts my_string
+puts my_float
