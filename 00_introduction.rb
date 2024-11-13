de1ff6b5a24537f97ad49bba569e38698c071498
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-11-13 11:30:47 +0530

    Introduction

---
+++ 00_introduction.rb
@@ -0,0 +1,10 @@
+#  Audience Requirements:
+#   - Knows basics of any one programming language
+#   - Especially useful for people who know Python,
+#     JavaScript, Kotlin or Java 
+
+#   Ruby is a dynamic, reflective, object-oriented,
+#     general-purpose programming language.
+#   Favours developers productivity over machine
+#   Feels like english language
+  
